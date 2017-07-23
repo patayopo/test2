939a2aa6e53675d4fb6c36b101180aace9060411
patayopo <aleho273@gmail.com>
2017-07-23 22:45:44 +0200

    add form

---
+++ formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Les formulaires</title>
+</head>
+    <body>
+        <form method = "POST" action = "target.php">
+            <p>
+                <label for = "prenom">Inscrivez votre prénom: </label> //le label indique à l'utilisateur quoi inscrire
+                <input type = "test" name = "prenom" id = "prenom"/>
+            </p>
+            <p>
+                <input type = "submit" value = "Envoyer"/> //bouton d'envoie
+            </p>
+        </form>
+    </body>
+</html>
